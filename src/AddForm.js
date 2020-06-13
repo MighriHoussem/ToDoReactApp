a675c713199ab28a8f80e80c0a3412f56193e3fe
MighriHoussem <mighri.houssem.official@gmail.com>
2020-06-13 18:37:35 +0200

    13-06-2020 bootstrap & JQuery with react

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,4 +1,5 @@
 import React , {Component} from "react";
+import $ from "jquery";
 
 
 class AddForm extends Component
@@ -8,43 +9,68 @@ class AddForm extends Component
         super(props);
         this.state={
             id: null,
-            content: null
+            content: ""
         };
     }
     handleChange =(event)=>
     {
         console.log(event.target.value);
+        $("#errorMessage").addClass("hide");
+        if(event.target.value.trim().length)
         this.setState({
-            [event.target.id] : event.target.value
+            [event.target.id] : event.target.value,
+            id : parseInt(Math.random()*100)
         });
 
     };
     handleSubmit = (event)=>
     {
         event.preventDefault();
-        this.state.id = parseInt(Math.random()*100);
         console.log(this.state);
+        if(this.state.content.trim().length)
+        {
+            //$("#errorMessage").addClass("hide");
+            this.props.addTodo(this.state);
+            this.setState({
+                id: null,
+                content : ""
+            });
+        }else
+        {
+            $("#errorMessage").removeClass("hide");
+        }
 
-        this.props.addTodo(this.state);
-        this.setState({
-            id: null,
-            content : null
-        });
-        document.getElementById("content").value="";
+        //document.getElementById("content").value="";
 
 
     };
+    handleErrorMessage = () => {
+
+        return <span>Please Enter your name</span>;
+    };
+    //fetch data to render component
+    componentDidMount() {
+    }
+    //fetch data & update component state
+    componentDidUpdate(prevProps, prevState, snapshot) {
+    }
+    //liberate component data
+    componentWillUnmount() {
+    }
+
     render()
     {
         return (
-            <div className={"container"}>
-                <form onSubmit={this.handleSubmit}>
+            <div className=" col-md-12">
+                <form className="col-md-12" onSubmit={this.handleSubmit}>
+                    <div className="alert alert-danger col-md-4 hide" id="errorMessage">{this.handleErrorMessage()}</div>
                     <label htmlFor={"content"}>Add a new Todo:</label>
                     <input
                         type = "text"
                         placeholder="Enter your name here"
                         id="content"
                         name="content"
+                        value = {this.state.content}
                         maxLength="20"
                         minLength="5"
                         onChange={this.handleChange}
